e039f2b69be0cde3af1615b22b9e433ec7697b6c
brusiqueira9 <106994736+brusiqueira9@users.noreply.github.com>
2022-06-12 12:39:09 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# Aplicando conceitos aprendidos na maratona premiada da Kenzie
+
+Projeto desenvolvido para o maratona premiada no 11 de Junho de 2022.
+
+
+### Estrutura de arquivos
+
+- README.md
+- index.html
+- /assets
+  - /img
+  - /css
+    - style.css
+  - /js
+    - script.js
+
+### Fonte
+
+- 'Poppins', sans-serif;
+
+## Layout do Projeto Original
+
+- [Layout Maratona premiada => Desenvolvendo restaurante Kenzie](https://www.figma.com/file/ONBBqq2qNpTSnixZHqUnSs/Maratona-Premiada---Kenzie?node-id=0%3A1)
+
+## Repositório do Projeto Original
+(https://github.com/Kenzie-Academy-Brasil-Developers/maratona_premiada)
+
+
+
+
---
+++ assets/script/database.js
@@ -0,0 +1,45 @@
+const products = [
+  {
+    id: "01",
+    title: "X-Burgão",
+    description: "Hambúrguer à base de aprendizado em desenvolvimento web!",
+    price: "28.00",
+    img: "./assets/img/x_burgao.png",
+  },
+  {
+    id: "02",
+    title: "Refrigerante",
+    description: "Coquinha gelada para acompanhar sua refeição!",
+    price: "7.50",
+    img: "./assets/img/refrigerante.png",
+  },
+  {
+    id: "03",
+    title: "X-Burguinho",
+    description:
+      "Delicioso hambúrguer acompanhado de um ovo frito de gema mole!",
+    price: "22.00",
+    img: "./assets/img/x_burguinho.png",
+  },
+  {
+    id: "04",
+    title: "Frango Frito",
+    description: "Frango frito e crocante com molho especial",
+    price: "25.00",
+    img: "./assets/img/frango_frito.png",
+  },
+  {
+    id: "05",
+    title: "Batata Frita",
+    description: "Batata frita acompanhada de molho especial",
+    price: "15.00",
+    img: "./assets/img/batata_frita.png",
+  },
+  {
+    id: "06",
+    title: "Sanduíche com Frango Frito",
+    description: "Pão, frango frito e salada para acompanhar",
+    price: "20.00",
+    img: "./assets/img/sanduiche_frango_frito.png",
+  },
+];
